5f9b7b21fbdc2885a369d15f01592a6a230460fa
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-05-25 11:23:47 -0700

    Basic JavaScript assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,4 @@
+window.alert("Warning! Fun ahead!"); //Creating a pop-up window message
+var Sent1 = "Everybody have ", Sent2 = "fun tonight!"; //defining two variables with strings
+document.write(Sent1 + Sent2); //concatenating the two strings
+
